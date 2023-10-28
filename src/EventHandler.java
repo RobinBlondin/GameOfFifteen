ba074315ee025ef5be75a33d8cd9e4ad91e0be1c
Robin Blondin <robin.blondin@protonmail.com>
2023-10-28 12:52:20 +0200

    Removed unusued imports

--- src/EventHandler.java
+++ src/EventHandler.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventHandler extends JFrame implements ActionListener {
