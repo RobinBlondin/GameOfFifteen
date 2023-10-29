6f3cd1b6ab3102d7f3e56916dd0abc806977921f
Robin Blondin <robin.blondin@protonmail.com>
2023-10-29 18:19:30 +0100

    Restructured code
    Added instance of utils class

--- src/EventHandler.java
+++ src/EventHandler.java
@@ -1,33 +1,33 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 public class EventHandler implements ActionListener {
     private final UI ui;
     private final Utils utils;
     private final Mechanics m;
 
-    public EventHandler(UI ui, Mechanics m) {
+    public EventHandler(UI ui, Utils utils, Mechanics m) {
         this.ui = ui;
+        this.utils = utils;
         this.m = m;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        if(m.createFlatList(ui.Lista).contains((JButton) e.getSource())) {
+        if(utils.createFlatList(ui.getButtonList()).contains((JButton) e.getSource())) {
             if(m.isButtonNextToEmpty((JButton) e.getSource())) {
-                ui.Lista = m.swapButtons(ui.Lista, (JButton) e.getSource());
-                revalidateRepaint();
+                ui.setButtonList(m.swapButtons(ui.getButtonList(), (JButton) e.getSource()));
+                ui.revalidateRepaint();
             }
-        } else if(e.getSource().equals(ui.shuffle)) {
-            m.shuffleList(ui.Lista);
-            rewriteBoard();
-            revalidateRepaint();
-        } else if (e.getSource().equals(ui.godMode)) {
-            m.sortList(ui.Lista);
-            rewriteBoard();
-            revalidateRepaint();
+        } else if(e.getSource().equals(ui.getShuffle())) {
+            utils.shuffleList(ui.getButtonList());
+            ui.rewriteBoard();
+            ui.revalidateRepaint();
+        } else if (e.getSource().equals(ui.getGodMode())) {
+            utils.sortList(ui.getButtonList());
+            ui.rewriteBoard();
+            ui.revalidateRepaint();
         }
     }
 }
