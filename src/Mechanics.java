4841dd54d60e98dd377bbdd35498d508765fa3f3
Robin Blondin <robin.blondin@protonmail.com>
2023-10-29 18:15:55 +0100

    Restructured order of methods in Mechanics

--- src/Mechanics.java
+++ src/Mechanics.java
@@ -5,8 +5,10 @@ public class Mechanics {
     private final UI ui;
     private final Utils utils;
 
-    public Mechanics(UI ui) {
+
+    public Mechanics(UI ui, Utils utils) {
         this.ui = ui;
+        this.utils = utils;
     }
 
     /**
@@ -28,6 +30,37 @@ public class Mechanics {
         }
         return null;
     }
+
+
+    /**
+     * Takes JButton as input and compares coordinates with the empty button. If the input button is next to
+     * the empty button this method will return true.
+     */
+    public boolean isButtonNextToEmpty(JButton clickedButton) {
+        int[] emptyPos = getPositionOfButton(ui.getButtonList(), "");
+        int[] clickedButtonPos = getPositionOfButton(ui.getButtonList(), clickedButton.getText());
+
+        int rowDifference = Math.abs(emptyPos[0] - clickedButtonPos[0]);
+        int columnDifference = Math.abs(emptyPos[1] - clickedButtonPos[1]);
+
+        return (rowDifference == 0 && columnDifference == 1) || (rowDifference == 1 && columnDifference == 0);
+    }
+
+
+    /**
+     * Iterates through a list of buttons and returns the first button that has no text.
+     */
+    public JButton findEmptyButton(List<List<JButton>> inputList) {
+        for (List<JButton> list : inputList) {
+            for(JButton button : list) {
+                if(button.getText().isEmpty()){
+                    return button;
+                }
+            }
+        }
+        return null;
+    }
+
 
     /**
      * Takes a 2d list and a button as input and swaps the text of the input button with the empty button
