658dd89a45efeece6b52448927f4500dbb41d080
Robin Blondin <robin.blondin@protonmail.com>
2023-10-29 18:15:28 +0100

    Moved list to Utils

--- src/Mechanics.java
+++ src/Mechanics.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Mechanics {
     private final UI ui;
-    private final List<String> numberList = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "");
+    private final Utils utils;
 
     public Mechanics(UI ui) {
         this.ui = ui;
