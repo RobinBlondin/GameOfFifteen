c3d05ab73b35f3ccc09081534cb89efd9e1e3805
Robin Blondin <robin.blondin@protonmail.com>
2023-10-30 11:59:31 +0100

    Restructured order of methods and added comments

--- src/Utils.java
+++ src/Utils.java
@@ -13,6 +13,9 @@ public class Utils extends JFrame {
         this.ui = ui;
     }
 
+    /**
+     * Takes a 2d array as input and creates and returns a list of lists.
+     */
     public List<List<JButton>> createListFromArray(JButton[][] array) {
         List<List<JButton>> list = new ArrayList<>();
         for (JButton[] jButtons : array) {
@@ -22,6 +25,10 @@ public class Utils extends JFrame {
     }
 
 
+    /**
+     * Takes a list of lists as input replace the text of each button in the order of the
+     * sorted list.
+     */
     public void sortList(List<List<JButton>> inputList) {
         List<JButton> buttons = createFlatList(inputList);
         for (int i = 0; i < buttons.size(); i++) {
@@ -31,39 +38,6 @@ public class Utils extends JFrame {
         ui.setButtonList(create2dList(buttons));
     }
 
-    public void shuffleList(List<List<JButton>> inputList) {
-        List<JButton> buttons = createFlatList(inputList);
-        List<String> shuffledList = createSolvableBoard(new ArrayList<>(numberList));
-
-        for (int i = 0; i < buttons.size(); i++) {
-            buttons.get(i).setText(shuffledList.get(i));
-            ui.resetButtonColor(i, buttons.get(i));
-        }
-
-        ui.setButtonList(create2dList(buttons));
-    }
-
-    /**
-     * Takes a 2d list as input and returns it as a normal list.
-     */
-    public List<JButton> createFlatList(List<List<JButton>> inputList) {
-        List<JButton> list = new ArrayList<>();
-        for(List<JButton> l : inputList) {
-            list.addAll(l);
-        }
-        return list;
-    }
-
-    /**
-     * Takes a list of JButton as input and creates and returns a 2d list.
-     */
-    public List<List<JButton>> create2dList(List<JButton> flatList) {
-        List<List<JButton>> list = new ArrayList<>();
-        for (int i = 0; i < flatList.size(); i += 4) {
-            list.add(flatList.subList(i, i+4).stream().toList());
-        }
-        return list;
-    }
 
     /**
      * Takes a list of strings as input and shuffles it until it is solvable.
@@ -85,6 +59,47 @@ public class Utils extends JFrame {
         }
         return list;
     }
+
+
+    /**
+     * Takes a list of lists as input and replace the text of the buttons
+     * with a new shuffled order.
+     */
+    public void shuffleList(List<List<JButton>> inputList) {
+        List<JButton> buttons = createFlatList(inputList);
+        List<String> shuffledList = createSolvableBoard(new ArrayList<>(numberList));
+
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setText(shuffledList.get(i));
+            ui.resetButtonColor(i, buttons.get(i));
+        }
+
+        ui.setButtonList(create2dList(buttons));
+    }
+
+    /**
+     * Takes a list of lists as input and flatten it to a 1d list.
+     */
+    public List<JButton> createFlatList(List<List<JButton>> inputList) {
+        List<JButton> list = new ArrayList<>();
+        for(List<JButton> l : inputList) {
+            list.addAll(l);
+        }
+        return list;
+    }
+
+    /**
+     * Takes a list of JButton as input and creates and returns a 2d list.
+     */
+    public List<List<JButton>> create2dList(List<JButton> flatList) {
+        List<List<JButton>> list = new ArrayList<>();
+        for (int i = 0; i < flatList.size(); i += 4) {
+            list.add(flatList.subList(i, i+4).stream().toList());
+        }
+        return list;
+    }
+
+
 
     public void validatePuzzle (List<JButton> inputList) {
 
